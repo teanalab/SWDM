9f37d450638d68262366496f8deeb9817b302c74
Saeid Balaneshinkordan <balaneshin.saeid@gmail.com>
2017-02-03 18:17:01 -0500

    unit tested gen_combine_fields_text

--- queries/test_queryLanguageModifier.py
+++ queries/test_queryLanguageModifier.py
@@ -21,3 +21,21 @@ class TestQueryLanguageModifier(TestCase):
         res = self.queryLanguageModifier.get_bigrams_list("hello world how are you")
         expected_res = ['hello world', 'world how', 'how are', 'are you']
         self.assertEqual(res, expected_res)
+
+    def test_gen_combine_fields_text(self):
+        field_weights = {
+            "u": 0.8,
+            "o": 0.1,
+            "w": 0.1
+        }
+        field_texts = {'u': '#combine(hello world how are you)',
+                       'w': '#combine( #uw17(hello world) #uw17(world how) #uw17(how are) #uw17(are you) )',
+                       'o': '#combine( #od4(hello world) #od4(world how) #od4(how are) #od4(are you) )'}
+
+        res = self.queryLanguageModifier.gen_combine_fields_text(field_weights, field_texts)
+        expected_res = '\n   #weight(\n      0.8#combine(hello world how are you)\n      ' + \
+                       '0.1#combine( #uw17(hello world) #uw17(world how) #uw17(how are) #uw17(are you) )\n      ' + \
+                       '0.1#combine( #od4(hello world) #od4(world how) #od4(how are) #od4(are you) )\n   )\n'
+
+
+        self.assertEqual(res, expected_res)
