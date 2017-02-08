feccf325c6585b05cba5136b1811928df77dda35
Saeid Balaneshinkordan <balaneshin.saeid@gmail.com>
2017-02-08 17:22:34 -0500

    added new count functions to the index class

--- index/index.py
+++ index/index.py
@@ -10,11 +10,27 @@ class Index:
         query = operator + str(window_size) + "(" + term + ")"
         return self.index.expression_count(query)
 
+    def document_expression_count(self, term, operator, window_size):
+        query = operator + str(window_size) + "(" + term + ")"
+        return self.index.document_expression_count(query)
+
     def uw_expression_count(self, term, window_size):
         return self.expression_count(term, "#uw", window_size)
 
     def od_expression_count(self, term, window_size):
         return self.expression_count(term, "#od", window_size)
+
+    def uw_document_expression_count(self, term, window_size):
+        return self.document_expression_count(term, "#uw", window_size)
+
+    def od_document_expression_count(self, term, window_size):
+        return self.document_expression_count(term, "#od", window_size)
+
+    def term_count(self, term):
+        return self.index.term_count(term)
+
+    def document_count(self, term):
+        return self.index.document_count(term)
 
 if __name__ == '__main__':
     pass
--- index/test_index.py
+++ index/test_index.py
@@ -12,3 +12,15 @@ class TestIndex(TestCase):
 
     def test_od_expression_count(self):
         self.assertEqual(self.index_.od_expression_count("SAMPSON True", 12), 1)
+
+    def test_uw_document_expression_count(self):
+        self.assertEqual(self.index_.uw_document_expression_count("SAMPSON True", 12), 1)
+
+    def test_od_document_expression_count(self):
+        self.assertEqual(self.index_.od_document_expression_count("SAMPSON True", 12), 1)
+
+    def test_term_count(self):
+        self.assertEqual(self.index_.term_count("dog"), 2)
+
+    def test_document_count(self):
+        self.assertEqual(self.index_.document_count("dog"), 1)
