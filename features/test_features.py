ad430cc47c95c95ab42e8c75619e531867ef6845
Saeid Balaneshinkordan <balaneshin.saeid@gmail.com>
2017-02-09 11:28:38 -0500

    unit-tested features class

--- features/test_features.py
+++ features/test_features.py
@@ -13,11 +13,31 @@ class TestIndex(TestCase):
             "od_expression_count": {
                 "window_size": 4
             },
+            "uw_document_expression_count": {
+                "window_size": 4
+            },
+            "od_document_expression_count": {
+                "window_size": 4
+            },
         }
 
     def test_uw_expression_count(self):
         self.assertEqual(self.features.uw_expression_count("SAMPSON Dog",
                                                            self.feature_parameters["uw_expression_count"]), 2)
+
+    def test_od_document_expression_count(self):
+        self.assertEqual(self.features.od_document_expression_count(
+            "SAMPSON True", self.feature_parameters["od_document_expression_count"]), 1)
+
+    def test_uw_document_expression_count(self):
+        self.assertEqual(self.features.uw_document_expression_count(
+            "SAMPSON True", self.feature_parameters["uw_document_expression_count"]), 1)
+
+    def test_document_count(self):
+        self.assertEqual(self.features.document_count("dog", {}), 1)
+
+    def test_term_count(self):
+        self.assertEqual(self.features.term_count("dog", {}), 2)
 
     def test_linear_combination(self):
         term = "SAMPSON Dog"
